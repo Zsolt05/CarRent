00657118c98d98c07a1aec67646c8081fb721fa8
lleila1122 <165311566+lleila1122@users.noreply.github.com>
2024-04-25 22:54:27 +0200

    Add files via upload
    IdentityFramework hasznalata helyett

---
+++ Controllers/CustomerUserStore.cs
@@ -0,0 +1,27 @@
+﻿using CarRent.Data;
+using CarRent.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRent.Controllers
+{
+    public class CustomUserStore
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomUserStore(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> FindByNameAsync(string userName)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+        }
+
+        public async Task<bool> CheckPasswordAsync(User user, string password)
+        {
+            return user.Password == password; 
+        }
+
+    }
+}
