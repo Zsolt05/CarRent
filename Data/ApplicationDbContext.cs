2afad9838fe8d31b71d3a27ec635a553b166419f
lleila1122 <165311566+lleila1122@users.noreply.github.com>
2024-04-25 22:49:25 +0200

    Update ApplicationDbContext.cs
    DbContext szarmaztatas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,38 +1,45 @@
 ﻿using CarRent.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRent.Data
 {
-    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
-    {
+    public class ApplicationDbContext : DbContext{
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+            public DbSet<User> Users { get; set; }
+            public DbSet<Rental> Rentals { get; set; }
+            public DbSet<Car> Cars { get; set; }
+            public DbSet<Category> Category { get; set; }
+            public DbSet<Sale> Sales { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Car>()
+                    .HasOne<Category>(r => r.Category)
+                    .WithMany(c => c.Cars)
+                    .HasForeignKey(r => r.CategoryID);
+
+                modelBuilder.Entity<Rental>()
+                    .HasOne<User>(l => l.User)
+                    .WithMany(u => u.Rentals)
+                    .HasForeignKey(l => l.UserID);
+
+                modelBuilder.Entity<Rental>()
+                    .HasOne<Car>(l => l.Car)
+                    .WithMany(r => r.Rentals)
+                    .HasForeignKey(l => l.CarID);
+
+                modelBuilder.Entity<Car>()
+                  .HasOne<Sale>(r => r.Sale)
+                  .WithMany(s => s.Cars)
+                  .HasForeignKey(r => r.SaleID);
+            }
         
-        public DbSet<User> Users { get; set; }
-        public DbSet<Rental> Rentals { get; set; }
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<Sale> Sales { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Car>()
-                .HasOne<Category>(r => r.Category)
-                .WithMany(c => c.Cars)
-                .HasForeignKey(r => r.CategoryID);
-
-            modelBuilder.Entity<Rental>()
-                .HasOne<User>(l => l.User)
-                .WithMany(u => u.Rentals)
-                .HasForeignKey(l => l.UserID);
-
-            modelBuilder.Entity<Rental>()
-                .HasOne<Car>(l => l.Car)
-                .WithMany(r => r.Rentals)
-                .HasForeignKey(l => l.CarID);
-
-            modelBuilder.Entity<Sale>()
-              .HasOne<Car>(e => e.Car)
-              .WithMany(r => r.Sales)
-              .HasForeignKey(e => e.CarID);
-        }
     }
 }
